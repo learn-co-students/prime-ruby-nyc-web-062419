3aa4ee42ec5d64f81c1c984df6a7e3c6be3ffda7
Luis Ascencio <luismiguelascencio@gmail.com>
2019-05-07 14:19:18 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+   
+if num < 0 then return false 
+    elsif num == 0 then return false 
+    elsif num == 1 then return false 
+else
+    (2..num-1).to_a.all? do |factor|
+        num % factor !=0
+    end 
+end 
+end 
+
