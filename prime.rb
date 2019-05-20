2599f85bc2cfc186bb60c6dba97e95876307da68
Colin <cstatro@gmail.com>
2019-05-20 01:23:58 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+def prime? number
+    i = 1
+    count =  0 
+    while i < number
+        count += 1 unless number % i != 0
+        i += 1   
+    end
+    count == 1
+end
+
+
+
+
