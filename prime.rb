9ad3f225230fb69fe30e1338bde29c909066519f
Anthony <Anthonywan00@gmail.com>
2019-05-30 12:08:12 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(num)
+    if num < 2
+        return false
+    else
+        x = num - 1
+        y =[]
+        while x >= 2
+            y << num % x
+            x -= 1
+        end
+        return !y.include?(0)
+    end
+end
+
