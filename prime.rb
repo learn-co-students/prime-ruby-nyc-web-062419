7b04a7027d86d42da8194fc75b6a3fa034df213c
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-11 16:25:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(int)
+  x = 2
+  prime_flag = true
+  if int <= 1
+    prime_flag = false
+  end
+  while x <= int / 2
+    if int % x == 0
+      prime_flag = false
+      break
+    end
+    x += 1
+  end
+  prime_flag
+end
