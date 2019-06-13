c10908a70945349a6f825885ee6762aff5f95a92
IanGrubb <yangrubb@gmail.com>
2019-06-13 16:15:32 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+
+def prime? (int)
+
+    if int < 0
+        int = -int
+    end
+
+    if int < 2
+        false
+    elsif int < 4
+        true
+    else
+        value = true
+        for pdiv in (2..(int/2)) do
+            if int % pdiv == 0
+                value = false
+                break
+            end
+        end
+        value
+    end
+    
+end
